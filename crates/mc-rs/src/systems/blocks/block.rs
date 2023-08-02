3d32700d780cfec4cd1c4b027ad58a73bbe01896
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-08-02 11:33:33 -0700

    Rework block textures Replace broken textures with fallback

--- crates/mc-rs/src/systems/blocks/block.rs
+++ crates/mc-rs/src/systems/blocks/block.rs
@@ -9,16 +9,27 @@ pub struct Block {
     pub id: u32,
     pub name: String,
     pub key: ResourceLocation,
-    pub textures: BlockTexture,
+    pub texture: BlockTexture,
 }
 
 impl Block {
-    pub(crate) fn new(id: u32, name: &str, textures: BlockTexture) -> Self {
+    /// Create a new block with the given id, name, and texture path(s)
+    pub fn new(id: u32, name: &str, paths: &[&str], assets: &AssetServer) -> Option<Self> {
+        Some(Self {
+            id,
+            name: name.to_case(Case::Title),
+            key: ResourceLocation::new(name.to_case(Case::Snake)),
+            texture: BlockTexture::from_paths(paths, assets)?,
+        })
+    }
+
+    /// Create a new block with the given id, name, and texture
+    pub fn new_with(id: u32, name: &str, texture: BlockTexture) -> Self {
         Self {
             id,
             name: name.to_case(Case::Title),
             key: ResourceLocation::new(name.to_case(Case::Snake)),
-            textures,
+            texture,
         }
     }
 }
@@ -29,82 +40,106 @@ pub enum BlockTexture {
     /// The same texture on all sides
     Single(Handle<Image>),
     /// The same texture on the top and bottom, and another texture on all sides
-    TopBottom(Handle<Image>, Handle<Image>),
+    TopBottom(Vec<Handle<Image>>),
     /// A different texture on the top, the bottom, and the sides
-    TopBottomSides(Handle<Image>, Handle<Image>, Handle<Image>),
+    TopBottomSides(Vec<Handle<Image>>),
     /// A different texture on the top, bottom, and sides
-    AllSides(
-        Handle<Image>,
-        Handle<Image>,
-        Handle<Image>,
-        Handle<Image>,
-        Handle<Image>,
-        Handle<Image>,
-    ),
+    AllSides(Vec<Handle<Image>>),
 }
 
 impl BlockTexture {
+    /// Get the texture for the given direction
     pub fn get_texture(&self, direction: Direction) -> Option<&Handle<Image>> {
         match self {
             BlockTexture::None => None,
             BlockTexture::Single(texture) => Some(texture),
-            BlockTexture::TopBottom(top_bot, side) => match direction {
-                Direction::Up | Direction::Down => Some(top_bot),
-                _ => Some(side),
+            BlockTexture::TopBottom(textures) => match direction {
+                Direction::Up | Direction::Down => Some(&textures[0]),
+                _ => Some(&textures[1]),
             },
-            BlockTexture::TopBottomSides(top, bot, side) => match direction {
-                Direction::Up => Some(top),
-                Direction::Down => Some(bot),
-                _ => Some(side),
+            BlockTexture::TopBottomSides(textures) => match direction {
+                Direction::Up => Some(&textures[0]),
+                Direction::Down => Some(&textures[1]),
+                _ => Some(&textures[2]),
             },
-            BlockTexture::AllSides(top, bot, north, south, west, east) => match direction {
-                Direction::Up => Some(top),
-                Direction::Down => Some(bot),
-                Direction::North => Some(north),
-                Direction::South => Some(south),
-                Direction::West => Some(west),
-                Direction::East => Some(east),
+            BlockTexture::AllSides(textures) => match direction {
+                Direction::Up => Some(&textures[0]),
+                Direction::Down => Some(&textures[1]),
+                Direction::North => Some(&textures[2]),
+                Direction::South => Some(&textures[3]),
+                Direction::West => Some(&textures[4]),
+                Direction::East => Some(&textures[5]),
             },
         }
     }
 
-    pub fn get_textures(&self) -> Option<Vec<&Handle<Image>>> {
+    /// Get all of the block textures
+    pub fn get_textures(&self) -> Option<&[Handle<Image>]> {
         match self {
             BlockTexture::None => None,
-            BlockTexture::Single(texture) => Some(vec![texture]),
-            BlockTexture::TopBottom(top_bot, side) => Some(vec![top_bot, side]),
-            BlockTexture::TopBottomSides(top, bot, side) => Some(vec![top, bot, side]),
-            BlockTexture::AllSides(top, bot, north, south, west, east) => {
-                Some(vec![top, bot, north, south, west, east])
-            }
+            BlockTexture::Single(texture) => Some(std::slice::from_ref(texture)),
+            BlockTexture::TopBottom(textures) => Some(textures),
+            BlockTexture::TopBottomSides(textures) => Some(textures),
+            BlockTexture::AllSides(textures) => Some(textures),
         }
     }
 
-    pub(crate) fn new_single(texture: &str, assets: &AssetServer) -> Self {
-        Self::Single(Self::load_image(texture, assets))
-    }
-
-    pub(crate) fn new_top_bottom(top_bottom: &str, sides: &str, assets: &AssetServer) -> Self {
-        Self::TopBottom(
-            Self::load_image(top_bottom, assets),
-            Self::load_image(sides, assets),
+    /// Creates a new block texture from a list of textures
+    pub fn from_paths(paths: &[&str], assets: &AssetServer) -> Option<Self> {
+        Self::from_textures(
+            paths
+                .iter()
+                .map(|&path| Self::load_test_texture(path, assets))
+                .collect(),
         )
     }
 
-    pub(crate) fn new_top_bottom_sides(
+    /// Creates a new block texture from a list of textures
+    pub fn from_textures(textures: Vec<Handle<Image>>) -> Option<Self> {
+        match textures.len() {
+            0 => Some(Self::None),
+            1 => Some(Self::Single(
+                textures
+                    .into_iter()
+                    .next()
+                    .expect("Texture list with length 1 but no texture?"),
+            )),
+            2 => Some(Self::TopBottom(textures)),
+            3 => Some(Self::TopBottomSides(textures)),
+            6 => Some(Self::AllSides(textures)),
+            _ => None,
+        }
+    }
+
+    /// Creates a new block texture from a single texture
+    pub fn new_single(texture: &str, assets: &AssetServer) -> Self {
+        Self::Single(Self::load_test_texture(texture, assets))
+    }
+
+    /// Creates a new block texture from a top/bottom texture and a side texture
+    pub fn new_top_bottom(top_bottom: &str, sides: &str, assets: &AssetServer) -> Self {
+        Self::TopBottom(vec![
+            Self::load_test_texture(top_bottom, assets),
+            Self::load_test_texture(sides, assets),
+        ])
+    }
+
+    /// Creates a new block texture from a top texture, bottom texture, and a side texture
+    pub fn new_top_bottom_sides(
         top: &str,
         bottom: &str,
         sides: &str,
         assets: &AssetServer,
     ) -> Self {
-        Self::TopBottomSides(
-            Self::load_image(top, assets),
-            Self::load_image(bottom, assets),
-            Self::load_image(sides, assets),
-        )
+        Self::TopBottomSides(vec![
+            Self::load_test_texture(top, assets),
+            Self::load_test_texture(bottom, assets),
+            Self::load_test_texture(sides, assets),
+        ])
     }
 
-    pub(crate) fn new_all_sides(
+    /// Creates a new block texture from a texture for each side
+    pub fn new_all_sides(
         top: &str,
         bottom: &str,
         north: &str,
@@ -113,17 +148,23 @@ impl BlockTexture {
         east: &str,
         assets: &AssetServer,
     ) -> Self {
-        Self::AllSides(
-            Self::load_image(top, assets),
-            Self::load_image(bottom, assets),
-            Self::load_image(north, assets),
-            Self::load_image(south, assets),
-            Self::load_image(west, assets),
-            Self::load_image(east, assets),
-        )
+        Self::AllSides(vec![
+            Self::load_test_texture(top, assets),
+            Self::load_test_texture(bottom, assets),
+            Self::load_test_texture(north, assets),
+            Self::load_test_texture(south, assets),
+            Self::load_test_texture(west, assets),
+            Self::load_test_texture(east, assets),
+        ])
     }
 
-    fn load_image(path: &str, assets: &AssetServer) -> Handle<Image> {
+    /// Shortcut for loading a test texture
+    fn load_test_texture(path: &str, assets: &AssetServer) -> Handle<Image> {
         assets.load(format!("test/textures/block/{path}"))
+    }
+
+    /// Shortcut for loading a texture
+    fn load_texture(path: &str, assets: &AssetServer) -> Handle<Image> {
+        assets.load(format!("textures/block/{path}"))
     }
 }
--- crates/mc-rs/src/systems/blocks/block_list.rs
+++ crates/mc-rs/src/systems/blocks/block_list.rs
@@ -3,10 +3,22 @@
 use bevy::{asset::LoadState, prelude::*, utils::HashMap};
 use mc_rs_proto::types::ResourceLocation;
 
+use crate::systems::states::application::MainMenuSet;
+
 use super::block::{Block, BlockTexture};
 
 /// Adds all block systems to the app
-pub(super) fn add_systems(app: &mut App) { app.add_systems(Startup, Blocks::init_blocks); }
+pub(super) fn add_systems(app: &mut App) {
+    app.init_resource::<BlocksLoaded>();
+    app.add_systems(Startup, Blocks::init_blocks);
+
+    app.add_systems(
+        Update,
+        BlocksLoaded::check_loaded
+            .in_set(MainMenuSet)
+            .run_if(resource_equals(BlocksLoaded(false)).and_then(resource_exists::<Blocks>())),
+    );
+}
 
 #[derive(Debug, Clone, Resource, Deref, DerefMut)]
 pub struct Blocks(pub HashMap<u32, Block>);
@@ -16,79 +28,55 @@ impl Blocks {
     fn init_blocks(mut commands: Commands, assets: Res<AssetServer>) {
         let mut blocks = Self(HashMap::new());
 
-        blocks.insert_block(0, "Air", BlockTexture::None);
-
-        blocks.insert_block(1, "Stone", BlockTexture::new_single("stone.png", &assets));
-
-        blocks.insert_block(
-            2,
-            "Granite",
-            BlockTexture::new_single("granite.png", &assets),
-        );
-        blocks.insert_block(
-            3,
-            "Polished Granite",
-            BlockTexture::new_single("polished_granite.png", &assets),
-        );
-
-        blocks.insert_block(
-            4,
-            "Diorite",
-            BlockTexture::new_single("diorite.png", &assets),
-        );
-        blocks.insert_block(
-            5,
-            "Polished Diorite",
-            BlockTexture::new_single("polished_diorite.png", &assets),
-        );
-
-        blocks.insert_block(
-            6,
-            "Andesite",
-            BlockTexture::new_single("andesite.png", &assets),
-        );
-        blocks.insert_block(
-            7,
-            "Polished Andesite",
-            BlockTexture::new_single("polished_andesite.png", &assets),
-        );
-
-        blocks.insert_block(
-            8,
-            "Grass Block",
-            BlockTexture::new_top_bottom_sides(
-                "grass_block_top.png",
-                "dirt.png",
-                "grass_block_side.png",
-                &assets,
-            ),
-        );
-        blocks.insert_block(9, "Dirt", BlockTexture::new_single("dirt.png", &assets));
-
-        for id in 22047..=22049 {
-            blocks.insert_block(
-                id,
-                "Deepslate",
-                BlockTexture::new_single("deepslate.png", &assets),
-            );
-        }
-
+        // Insert the error block
         blocks.insert(
             u32::MAX,
             Block {
                 id: u32::MAX,
                 name: "Error".to_string(),
                 key: ResourceLocation::new("mc-rs:error"),
-                textures: BlockTexture::new_single("light_blue_wool.png", &assets),
+                texture: BlockTexture::from_paths(&["light_blue_wool.png"], &assets).unwrap(),
             },
         );
+
+        blocks.insert_block(0, "Air", &Vec::new(), &assets);
+
+        blocks.insert_block(1, "Stone", &["stone.png"], &assets);
+
+        blocks.insert_block(2, "Granite", &["granite.png"], &assets);
+        blocks.insert_block(3, "Polished Granite", &["polished_granite.png"], &assets);
+
+        blocks.insert_block(4, "Diorite", &["diorite.png"], &assets);
+        blocks.insert_block(5, "Polished Diorite", &["polished_diorite.png"], &assets);
+
+        blocks.insert_block(6, "Andesite", &["andesite.png"], &assets);
+        blocks.insert_block(7, "Polished Andesite", &["polished_andesite.png"], &assets);
+
+        blocks.insert_block(
+            8,
+            "Grass Block",
+            &["grass_block_top.png", "dirt.png", "grass_block_side.png"],
+            &assets,
+        );
+        blocks.insert_block(9, "Dirt", &["dirt.png"], &assets);
+
+        for id in 22047..=22049 {
+            blocks.insert_block(id, "Deepslate", &["deepslate.png"], &assets);
+        }
 
         commands.insert_resource(blocks);
     }
 
     /// Insert a block into the block list
-    fn insert_block(&mut self, id: u32, name: &str, textures: BlockTexture) {
-        self.insert(id, Block::new(id, name, textures));
+    fn insert_block(&mut self, id: u32, name: &str, paths: &[&str], assets: &AssetServer) {
+        if let Some(block) = Block::new(id, name, paths, assets) {
+            self.insert(id, block);
+        } else {
+            error!("Failed to create block with id {}", id);
+
+            let fallback = self.get(&u32::MAX).unwrap().texture.clone();
+            self.insert(id, Block::new_with(id, name, fallback));
+        }
     }
 
     /// Returns true if all the block textures are loaded
@@ -98,23 +86,83 @@ impl Blocks {
     }
 
     /// Return the progress of loading the block textures
+    ///
+    /// Returns a tuple of blocks with loaded textures and total blocks with textures
     pub fn get_progress(&self, assets: &AssetServer) -> (u32, u32) {
         // Get the number of blocks with all textures loaded
         let blocks_loaded = self.values().fold(0u32, |acc, block| {
-            let Some(textures) = block.textures.get_textures() else {
+            let Some(textures) = block.texture.get_textures() else {
                 return acc;
             };
 
             let ids = textures.iter().map(|t| t.id());
-            acc + matches!(assets.get_group_load_state(ids), LoadState::Loaded) as u32
+            acc + matches!(
+                assets.get_group_load_state(ids),
+                LoadState::Loaded | LoadState::Failed
+            ) as u32
         });
 
         // Get the number of blocks with textures
         let blocks_with_textures = self
             .values()
-            .filter(|b| b.textures.get_textures().is_some())
+            .filter(|b| b.texture.get_textures().is_some())
             .count() as u32;
 
         (blocks_loaded, blocks_with_textures)
+    }
+
+    /// Replaces all failed block textures with the error block texture
+    ///
+    /// Returns the number of blocks that were fixed
+    pub fn replace_errors(&mut self, assets: &AssetServer) -> u32 {
+        let fallback = self.get(&u32::MAX).unwrap().texture.clone();
+        let mut acc = 0;
+
+        for block in self.values_mut() {
+            if let Some(textures) = block.texture.get_textures() {
+                // Check if any of the textures failed to load
+                let ids = textures.iter().map(|t| t.id());
+                if assets.get_group_load_state(ids) == LoadState::Failed {
+                    // Replace the block texture with the error block texture
+                    block.texture = fallback.clone();
+                    acc += 1;
+                }
+            }
+        }
+
+        acc
+    }
+}
+
+/// A resource that is true when all blocks are loaded
+#[derive(Debug, Default, Clone, PartialEq, Eq, Resource, Deref, DerefMut)]
+pub struct BlocksLoaded(pub bool);
+
+impl BlocksLoaded {
+    /// A system that checks if all the block textures are loaded
+    /// and replaces any broken textures with the fallback
+    fn check_loaded(
+        mut blocks: ResMut<Blocks>,
+        mut loaded: ResMut<BlocksLoaded>,
+        assets: Res<AssetServer>,
+    ) {
+        let (l, t) = blocks.get_progress(&assets);
+
+        if l == t {
+            // Replace any failed textures with the error block
+            let fixed = blocks.replace_errors(&assets);
+
+            if fixed > 0 {
+                // TODO: Some sort of error popup?
+                error!("{fixed} blocks failed to load textures");
+            } else {
+                info!("All blocks ({t}) loaded successfully");
+            }
+
+            // Set the blocks loaded resource to true
+            **loaded = true;
+        } else {
+            info!("Loaded {} of {} blocks", l, t);
+        }
     }
 }
