7ef00ad2484ca740a4d913575e9acf15d41d7143
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-09-02 13:53:23 -0700

    Add a couple blockstates

--- crates/mc-rs/src/systems/blocks/state/list.rs
+++ crates/mc-rs/src/systems/blocks/state/list.rs
@@ -51,4 +51,25 @@ pub(super) fn create_states(states: &mut StatesMap, asset_server: &AssetServer) 
     add_state!(states, asset_server, 0u32, 0u32, EMPTY);
     add_state!(states, asset_server, 1u32, 1u32, &["stone.png"]);
     add_state!(states, asset_server, 2u32, 2u32, &["granite.png"]);
+    add_state!(states, asset_server, 3u32, 3u32, &["polished_granite.png"]);
+    add_state!(states, asset_server, 4u32, 4u32, &["diorite.png"]);
+    add_state!(states, asset_server, 5u32, 5u32, &["polished_diorite.png"]);
+    add_state!(states, asset_server, 6u32, 6u32, &["andesite.png"]);
+    add_state!(states, asset_server, 7u32, 7u32, &["polished_andesite.png"]);
+    add_state_range!(
+        states,
+        asset_server,
+        8u32,
+        8u32..=9u32,
+        &["grass_block_top.png", "dirt.png", "grass_block_side.png"]
+    );
+    add_state!(states, asset_server, 9u32, 10u32, &["dirt.png"]);
+    add_state!(states, asset_server, 10u32, 11u32, &["coarse_dirt.png"]);
+    add_state_range!(
+        states,
+        asset_server,
+        11u32,
+        12u32..=13u32,
+        &["podzol_top.png", "dirt.png", "podzol_side.png"]
+    );
 }
