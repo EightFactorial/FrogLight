3cb9814e9cea2155ab7a52d38faa855020837516
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-10-10 13:26:27 -0700

    Organize some plugins

---
+++ src/plugins/default.rs
@@ -0,0 +1,84 @@
+use bevy::{
+    app::PluginGroupBuilder,
+    prelude::*,
+    render::{render_resource::AddressMode, texture::ImageSampler},
+    window::ExitCondition,
+};
+use rand::seq::IteratorRandom;
+
+use crate::systems::settings::Settings;
+
+/// Configure the default Bevy [DefaultPlugins].
+pub(crate) fn default_plugins(settings: &Settings) -> PluginGroupBuilder {
+    let mut plugins = DefaultPlugins.build();
+
+    // Enable asset hot-reloading
+    #[cfg(any(debug_assertions, feature = "debug"))]
+    {
+        use bevy::asset::ChangeWatcher;
+        use std::time::Duration;
+
+        plugins = plugins.set(AssetPlugin {
+            watch_for_changes: ChangeWatcher::with_delay(Duration::from_secs(1)),
+            ..default()
+        });
+    }
+
+    // Set the ImagePlugin sampling to nearest
+    {
+        let mut default_sampler = ImageSampler::nearest_descriptor();
+        default_sampler.address_mode_u = AddressMode::Repeat;
+        default_sampler.address_mode_v = AddressMode::Repeat;
+        default_sampler.address_mode_w = AddressMode::Repeat;
+
+        plugins = plugins.set(ImagePlugin { default_sampler })
+    }
+
+    // Set the WindowPlugin window settings
+    {
+        let title: String;
+
+        #[cfg(any(debug_assertions, feature = "debug"))]
+        {
+            title = format!(
+                "MC-RS v{} - nightly {} - {}",
+                env!("CARGO_PKG_VERSION"),
+                env!("VERGEN_GIT_DESCRIBE"),
+                get_title()
+            );
+        }
+        #[cfg(not(any(debug_assertions, feature = "debug")))]
+        {
+            title = format!("MC-RS v{} - {}", env!("CARGO_PKG_VERSION"), get_title());
+        }
+
+        let window = Window {
+            present_mode: settings.window.present_mode,
+            mode: settings.window.window_mode,
+            resolution: settings.window.resolution.clone(),
+            title,
+            ..default()
+        };
+
+        plugins = plugins.set(WindowPlugin {
+            primary_window: Some(window),
+            exit_condition: ExitCondition::OnPrimaryClosed,
+            close_when_requested: true,
+        });
+    }
+
+    plugins
+}
+
+static WINDOW_TITLES: &str = include_str!("../../assets/texts/titles.txt");
+
+/// Get a random window title.
+///
+/// All occurrences of `\n` will be replaced with a newline.
+fn get_title() -> String {
+    WINDOW_TITLES
+        .lines()
+        .choose(&mut rand::thread_rng())
+        .unwrap_or(WINDOW_TITLES.lines().next().unwrap())
+        .replace("\\n", "\n")
+}
--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,21 +1,14 @@
 use belly::prelude::BellyPlugin;
-use bevy::{
-    app::PluginGroupBuilder,
-    prelude::*,
-    render::{render_resource::AddressMode, texture::ImageSampler},
-    window::ExitCondition,
-};
+use bevy::prelude::*;
 use bevy_rapier3d::prelude::RapierPhysicsPlugin;
-use rand::seq::IteratorRandom;
+
+mod default;
 
 #[cfg(feature = "splash")]
 pub(crate) mod splash;
 
 #[cfg(feature = "debug")]
 mod debug;
-
-mod mc_rs;
-use mc_rs::MCRSPlugins;
 
 use crate::systems::settings::Settings;
 
@@ -26,7 +19,7 @@ pub(super) fn add_plugins(app: &mut App) {
     let settings = Settings::load();
 
     // Add default plugins
-    default_plugins(&settings).finish(app);
+    default::default_plugins(&settings).finish(app);
     app.insert_resource(settings);
 
     // Add Belly plugin
@@ -41,83 +34,18 @@ pub(super) fn add_plugins(app: &mut App) {
         app.add_plugins(RapierDebugRenderPlugin::default());
     }
 
-    // Add custom plugins
-    MCRSPlugins.build().finish(app);
-}
+    #[cfg(feature = "splash")]
+    {
+        use splash::SplashPlugin;
+        app.add_plugins(SplashPlugin);
+    }
 
-/// Configure the default Bevy [DefaultPlugins].
-fn default_plugins(settings: &Settings) -> PluginGroupBuilder {
-    let mut plugins = DefaultPlugins.build();
-
-    // Enable asset hot-reloading
     #[cfg(feature = "debug")]
     {
-        use bevy::asset::ChangeWatcher;
-        use std::time::Duration;
+        use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+        app.add_plugins(FrameTimeDiagnosticsPlugin);
 
-        plugins = plugins.set(AssetPlugin {
-            watch_for_changes: ChangeWatcher::with_delay(Duration::from_secs(1)),
-            ..default()
-        });
+        use debug::DebugPlugin;
+        app.add_plugins(DebugPlugin);
     }
-
-    // Set the ImagePlugin sampling to nearest
-    {
-        let mut default_sampler = ImageSampler::nearest_descriptor();
-        default_sampler.address_mode_u = AddressMode::Repeat;
-        default_sampler.address_mode_v = AddressMode::Repeat;
-        default_sampler.address_mode_w = AddressMode::Repeat;
-
-        plugins = plugins.set(ImagePlugin { default_sampler })
-    }
-
-    // Set the WindowPlugin window settings
-    {
-        let title: String;
-
-        #[cfg(any(debug_assertions, feature = "debug"))]
-        {
-            title = format!(
-                "MC-RS v{} - nightly {} - {}",
-                env!("CARGO_PKG_VERSION"),
-                env!("VERGEN_GIT_DESCRIBE"),
-                get_title()
-            );
-        }
-        #[cfg(not(any(debug_assertions, feature = "debug")))]
-        {
-            title = format!("MC-RS v{} - {}", env!("CARGO_PKG_VERSION"), get_title());
-        }
-
-        let window = Window {
-            present_mode: settings.window.present_mode,
-            mode: settings.window.window_mode,
-            resolution: settings.window.resolution.clone(),
-            title,
-            ..default()
-        };
-
-        plugins = plugins.set(WindowPlugin {
-            primary_window: Some(window),
-            exit_condition: ExitCondition::OnPrimaryClosed,
-            close_when_requested: true,
-        });
-    }
-
-    plugins
-}
-
-const WINDOW_TITLES: &str = include_str!("../../assets/texts/titles.txt");
-
-/// Get a random window title.
-///
-/// All occurrences of `\n` will be replaced with a newline.
-fn get_title() -> String {
-    let mut rng = rand::thread_rng();
-
-    WINDOW_TITLES
-        .lines()
-        .choose(&mut rng)
-        .unwrap_or(WINDOW_TITLES.lines().next().unwrap())
-        .replace("\\n", "\n")
 }
