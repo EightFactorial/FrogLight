eed88c890e5ac591a92db69b8c22a626df877d99
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-10-21 21:18:33 -0700

    Fix entity status packet

--- crates/mc-rs-protocol/src/versions/v1_20_0/play/clientboundentitystatuspacket.rs
+++ crates/mc-rs-protocol/src/versions/v1_20_0/play/clientboundentitystatuspacket.rs
@@ -1,10 +1,8 @@
 use mc_rs_macros::Transcode;
 
-use crate::types::EntityId;
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Transcode)]
 #[mctest(tests = ["transcode", "decode"], bytes = [1, 1])]
 pub struct ClientboundEntityStatusPacket {
-    pub entity_id: EntityId,
+    pub entity_id: u32,
     pub event_id: u8,
 }
