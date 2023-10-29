ec08454bd4b0f6a52115b0ee5e521d05a91f211c
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-10-28 17:20:35 -0700

    Add serde to workspace dependencies

--- crates/mc-rs-interface/src/settings/keybinds/mod.rs
+++ crates/mc-rs-interface/src/settings/keybinds/mod.rs
@@ -10,9 +10,13 @@ pub mod inventory;
 use inventory::InventoryKeybinds;
 
 // TODO: Add the ability to set keybinds to keyboard or mouse buttons
+// Maybe https://github.com/leafwing-studios/leafwing-input-manager ?
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Keybinds {
+    #[serde(default)]
     pub movement: MovementKeybinds,
+    #[serde(default)]
     pub gameplay: GameplayKeybinds,
+    #[serde(default)]
     pub inventory: InventoryKeybinds,
 }
