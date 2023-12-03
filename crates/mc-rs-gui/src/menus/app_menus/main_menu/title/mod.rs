7cdc7abeb5e38a5a9389bb972f7da84ceef69819
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-12-02 17:21:57 -0800

    Forgot to port the title, haha

--- crates/mc-rs-gui/src/menus/app_menus/main_menu/title/mod.rs
+++ crates/mc-rs-gui/src/menus/app_menus/main_menu/title/mod.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
+use mc_rs_resourcepack::assets::resourcepacks::AssetFromWorld;
 
-use crate::menus::traits::MenuComponent;
+use crate::{
+    menus::traits::{AddMenuResource, MenuComponent},
+    resources::scale::GuiScaleComponent,
+};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Component)]
 pub struct TitleNodeComponent;
@@ -11,15 +15,74 @@ impl MenuComponent for TitleNodeComponent {
     fn build(parent: Entity, world: &mut World) {
         #[cfg(any(debug_assertions, feature = "debug"))]
         trace!("Building TitleNodeComponent");
-        let node = NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        };
-        world.spawn((TitleNodeComponent, node)).set_parent(parent);
+
+        // Get the title texture
+        let title = world
+            .get_texture_or_fallback("minecraft:gui/title/minecraft")
+            .clone();
+        world.add_menu_resource(title.clone().untyped());
+
+        // Get the edition texture
+        let edition = world
+            .get_texture_or_fallback("minecraft:gui/title/edition")
+            .clone();
+        world.add_menu_resource(edition.clone().untyped());
+
+        let node = world
+            .spawn((
+                TitleNodeComponent,
+                NodeBundle {
+                    style: Style {
+                        position_type: PositionType::Absolute,
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        top: Val::Percent(10.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                #[cfg(any(debug_assertions, feature = "debug"))]
+                Outline {
+                    color: Color::BLUE,
+                    width: Val::Px(1.0),
+                    ..Default::default()
+                },
+            ))
+            .set_parent(parent)
+            .id();
+
+        let outer_title = world
+            .spawn((
+                GuiScaleComponent::new(256, 64),
+                ImageBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::End,
+                        align_items: AlignItems::Center,
+                        margin: UiRect::all(Val::Px(5.0)),
+                        ..Default::default()
+                    },
+                    image: title.into(),
+                    ..Default::default()
+                },
+            ))
+            .set_parent(node)
+            .id();
+
+        // Inner title
+        world
+            .spawn((
+                GuiScaleComponent::new(128, 16),
+                ImageBundle {
+                    style: Style {
+                        margin: UiRect::bottom(Val::Percent(4.0)),
+                        ..Default::default()
+                    },
+                    image: edition.into(),
+                    ..Default::default()
+                },
+            ))
+            .set_parent(outer_title);
     }
 }
