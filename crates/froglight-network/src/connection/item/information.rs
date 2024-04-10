1b1af49608b549dd152370701ef4943b6ffe8a18
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2024-04-10 10:10:43 -0700

    Allow receiving packets and getting the connection address

--- crates/froglight-network/src/connection/item/information.rs
+++ crates/froglight-network/src/connection/item/information.rs
@@ -1,12 +1,27 @@
 use std::net::SocketAddr;
 
-use compact_str::CompactString;
+use compact_str::{CompactString, ToCompactString};
 
 /// Information about a connection.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct ConnectionInformation {
     /// The address of the remote host.
+    ///
+    /// If the server ever asks for the address the client is connected to, this
+    /// is the address that will be sent.
     pub address: Option<CompactString>,
     /// The socket address of the remote host.
+    ///
+    /// This is the address that the connection is established with.
     pub socket: SocketAddr,
+}
+
+impl ConnectionInformation {
+    /// Gets the address of the connection.
+    ///
+    /// If the address is not set, the socket address is returned.
+    #[must_use]
+    pub fn get_address(&self) -> CompactString {
+        self.address.clone().unwrap_or_else(|| self.socket.to_compact_string())
+    }
 }
--- crates/froglight-network/src/connection/item/packets.rs
+++ crates/froglight-network/src/connection/item/packets.rs
@@ -1,7 +1,11 @@
-use async_compression::futures::write::ZlibEncoder;
-use async_std::io::WriteExt;
+use std::{collections::VecDeque, io::Cursor};
+
+use async_compression::futures::{bufread::ZlibDecoder, write::ZlibEncoder};
+use async_std::io::{ReadExt, WriteExt};
+use bevy_log::error;
+use bevy_tasks::futures_lite::AsyncBufReadExt;
 use froglight_protocol::{
-    protocol::{FrogVarWrite, FrogWrite, WriteError},
+    protocol::{FrogRead, FrogVarRead, FrogVarWrite, FrogWrite, ReadError, WriteError},
     traits::{State, Version},
 };
 
@@ -62,6 +66,102 @@ where
         buf.len().fg_var_write(&mut prefixed_buffer)?;
         prefixed_buffer.append(buf);
         *buf = prefixed_buffer;
+        Ok(())
+    }
+
+    /// Receive a packet from the other side of the connection.
+    ///
+    /// # Errors
+    /// If a packet cannot be received.
+    ///
+    /// # Panics
+    /// If the packet length overflows.
+    pub async fn recv(&mut self) -> Result<D::Recv, ConnectionError> {
+        // If there are any bundled packets, return the first one
+        if let Some(packet) = self.bundle.pop_front() {
+            return Ok(packet);
+        }
+
+        // Get the byte buffer
+        let buffer = self.buffer.fill_buf().await?;
+        let buffer_len = buffer.len();
+
+        // If the buffer is empty, the connection is closed
+        if buffer_len == 0 {
+            return Err(ConnectionError::ConnectionClosed);
+        }
+
+        // TODO: Decryption
+
+        // Read the packet length from the buffer
+        let mut cursor = Cursor::new(buffer);
+        let packet_length = usize::fg_var_read(&mut cursor)?;
+
+        // Consume the packet bytes from the buffer
+        let packet_length_bytes =
+            usize::try_from(cursor.position()).expect("Packet length overflow");
+
+        // If the packet is compressed, decompress it
+        if self.compression.is_some_and(|c| c >= 0) && 0 != u32::fg_var_read(&mut cursor)? {
+            // Decompress the packet
+            let mut decompressor = ZlibDecoder::new(
+                &cursor.get_ref()[packet_length_bytes..packet_length_bytes + packet_length],
+            );
+            let mut decompressed = Vec::with_capacity(packet_length);
+            decompressor.read_to_end(&mut decompressed).await?;
+
+            // Get the length of the decompressed packet
+            let decompressed_packet_length = decompressed.len();
+
+            // Read the packet from the decompressed buffer
+            let mut cursor = Cursor::new(decompressed.as_slice());
+            let packet = D::Recv::fg_read(&mut cursor);
+
+            // Read any bundled packets from the decompressed buffer
+            if packet.is_ok() && cursor.position() == 0u64 {
+                if let Err(err) =
+                    Self::read_bundled(decompressed_packet_length, &mut cursor, &mut self.bundle)
+                {
+                    error!("Error reading bundled packets: {err}");
+                }
+            }
+
+            // Consume the packet bytes from the original buffer
+            self.buffer.consume(packet_length_bytes + packet_length);
+
+            // Return the packet
+            packet.map_err(ConnectionError::from)
+        } else {
+            // Read the packet from the buffer
+            let packet = D::Recv::fg_read(&mut cursor);
+
+            // Read any bundled packets from the buffer
+            if packet.is_ok() && cursor.position() == 0u64 {
+                if let Err(err) = Self::read_bundled(packet_length, &mut cursor, &mut self.bundle) {
+                    error!("Error reading bundled packets: {err}");
+                }
+            }
+
+            // Consume the packet bytes from the buffer
+            self.buffer.consume(packet_length_bytes + packet_length);
+
+            // Return the packet
+            packet.map_err(ConnectionError::from)
+        }
+    }
+
+    /// Attempt to read bundled packets from the given cursor.
+    fn read_bundled(
+        packet_length: usize,
+        cursor: &mut Cursor<&[u8]>,
+        bundle: &mut VecDeque<D::Recv>,
+    ) -> Result<(), ReadError> {
+        while packet_length > usize::try_from(cursor.position()).expect("Packet length overflow") {
+            match D::Recv::fg_read(cursor) {
+                Ok(packet) => bundle.push_back(packet),
+                Err(err) => return Err(err),
+            }
+        }
         Ok(())
     }
 }
