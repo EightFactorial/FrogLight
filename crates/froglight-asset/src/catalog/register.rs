2e661f7eecb3bb3e6d96cdfeaa9348ec536c95d2
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2024-07-26 13:28:24 -0700

    Update `AssetKey` assets when the key is modified Observers don't trigger with change detection

--- crates/froglight-asset/src/catalog/register.rs
+++ crates/froglight-asset/src/catalog/register.rs
@@ -5,13 +5,14 @@ use bevy_asset::{Asset, Assets};
 use bevy_ecs::{
     entity::Entity,
     prelude::any_with_component,
+    query::Changed,
     schedule::IntoSystemConfigs,
     system::{Commands, Query, Res, ResMut},
 };
 use bevy_log::error;
 use bevy_state::state::OnEnter;
 
-use super::{systemset::AssetKeyRefreshSet, AssetCatalog, AssetKey};
+use super::{catalog::UntypedAssetMap, systemset::AssetKeyRefreshSet, AssetCatalog, AssetKey};
 use crate::AssetState;
 
 /// Add systems for refreshing [`AssetKey`]s of type `A`.
@@ -31,6 +32,32 @@ pub(super) fn add_systems<A: Asset>(app: &mut App) {
             .run_if(any_with_component::<AssetKey<A>>)
             .in_set(AssetKeyRefreshSet),
     );
+
+    // Add the change detection system to the `AssetState::Loaded` set
+    app.add_systems(
+        Update,
+        refresh_changed_asset_keys::<A>
+            .run_if(any_with_component::<AssetKey<A>>)
+            .run_if(any_keys_changed::<A>)
+            .in_set(AssetState::Loaded),
+    );
+}
+
+/// Returns `true` if any [`AssetKey`]s of type `A` have changed.
+fn any_keys_changed<A: Asset>(query: Query<(), Changed<AssetKey<A>>>) -> bool { query.is_empty() }
+
+/// Refresh the handles for entities with a changed [`AssetKey`].
+fn refresh_changed_asset_keys<A: Asset>(
+    query: Query<(Entity, &AssetKey<A>), Changed<AssetKey<A>>>,
+    catalog: Res<AssetCatalog>,
+    mut assets: ResMut<Assets<A>>,
+    mut commands: Commands,
+) {
+    let Some(untyped_map) = catalog.inner.get(&TypeId::of::<A>()) else {
+        error!("No \"{}\" Assets exist in the AssetCatalog!", A::short_type_path());
+        return;
+    };
+    refresh_keys(query.iter(), untyped_map, &mut assets, &mut commands);
 }
 
 /// Update the handles for all entities with an [`AssetKey`].
@@ -44,8 +71,16 @@ fn refresh_asset_keys<A: Asset>(
         error!("No \"{}\" Assets exist in the AssetCatalog!", A::short_type_path());
         return;
     };
+    refresh_keys(query.iter(), untyped_map, &mut assets, &mut commands);
+}
 
-    for (entity, key) in &query {
+fn refresh_keys<'a, A: Asset>(
+    iterator: impl Iterator<Item = (Entity, &'a AssetKey<A>)>,
+    untyped_map: &UntypedAssetMap,
+    assets: &mut Assets<A>,
+    commands: &mut Commands,
+) {
+    for (entity, key) in iterator {
         if let Some(untyped_id) = untyped_map.get(key.as_ref()) {
             if let Some(asset_handle) =
                 assets.get_strong_handle(untyped_id.typed_debug_checked::<A>())
