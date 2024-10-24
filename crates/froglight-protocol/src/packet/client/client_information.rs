313bbfced524f495269b793b9564ef97e050926b
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2024-10-23 19:28:13 -0700

    Start deleting packets unchanged from v1.21.0 Update v1.21.2 generation to be based on the v1.21.3 jar

--- crates/froglight-protocol/src/packet/client/client_information.rs
+++ crates/froglight-protocol/src/packet/client/client_information.rs
@@ -2,21 +2,6 @@
 
 use froglight_macros::FrogReadWrite;
 
-use crate::{common::PlayerHand, packet::flags::PlayerModelFlags};
-
-/// Client settings sent to the server.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, FrogReadWrite)]
-pub struct ClientSettings {
-    pub language: String,
-    pub view_distance: u8,
-    pub chat_visibility: ChatVisibility,
-    pub chat_colors: bool,
-    pub model_customization: PlayerModelFlags,
-    pub main_hand: PlayerHand,
-    pub text_filtering_enabled: bool,
-    pub allows_listing: bool,
-}
-
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FrogReadWrite)]
 #[frog(tests = ["read_verify", "write_verify"], bytes = [0])]
 pub enum ChatVisibility {
@@ -24,4 +9,13 @@ pub enum ChatVisibility {
     All,
     System,
     None,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FrogReadWrite)]
+#[frog(tests = ["read_verify", "write_verify"], bytes = [0])]
+pub enum ParticleMode {
+    #[default]
+    All,
+    Decreased,
+    Minimal,
 }
--- crates/froglight-protocol/src/versions/v1_21_0/play/client_options.rs
+++ crates/froglight-protocol/src/versions/v1_21_0/play/client_options.rs
@@ -1,10 +1,19 @@
-use derive_more::{Deref, DerefMut, From, Into};
 use froglight_macros::FrogReadWrite;
 
-use crate::packet::ClientSettings;
+use crate::{
+    common::PlayerHand,
+    packet::{ChatVisibility, PlayerModelFlags},
+};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deref, DerefMut, From, Into, FrogReadWrite)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, FrogReadWrite)]
 #[cfg_attr(feature = "bevy", derive(bevy_reflect::Reflect))]
 pub struct ClientOptionsPacket {
-    pub options: ClientSettings,
+    pub language: String,
+    pub view_distance: u8,
+    pub chat_visibility: ChatVisibility,
+    pub chat_colors: bool,
+    pub model_customization: PlayerModelFlags,
+    pub main_hand: PlayerHand,
+    pub text_filtering_enabled: bool,
+    pub allows_listing: bool,
 }
