cd335b82adab689f39087ae99107ac2ff0ef6fc8
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2024-12-05 17:02:51 -0800

    Derive `Reflect` on more types

--- crates/froglight-protocol/src/common/other/intent.rs
+++ crates/froglight-protocol/src/common/other/intent.rs
@@ -1,7 +1,11 @@
+#[cfg(feature = "bevy")]
+use bevy_reflect::std_traits::ReflectDefault;
 use froglight_macros::FrogReadWrite;
 
 /// The intent of a new connection.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FrogReadWrite)]
+#[cfg_attr(feature = "bevy", derive(bevy_reflect::Reflect))]
+#[cfg_attr(feature = "bevy", reflect(Default))]
 #[frog(tests = ["read_verify", "write_verify"], bytes = [2])]
 pub enum ConnectionIntent {
     /// The connection wants to get the status of the server.
--- crates/froglight-protocol/src/common/other/player_hand.rs
+++ crates/froglight-protocol/src/common/other/player_hand.rs
@@ -1,7 +1,11 @@
+#[cfg(feature = "bevy")]
+use bevy_reflect::std_traits::ReflectDefault;
 use froglight_macros::FrogReadWrite;
 
 /// The player's hand.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FrogReadWrite)]
+#[cfg_attr(feature = "bevy", derive(bevy_reflect::Reflect))]
+#[cfg_attr(feature = "bevy", reflect(Default))]
 #[frog(tests = ["read_verify", "write_verify"], bytes = [1])]
 pub enum PlayerHand {
     /// The player's left hand
@@ -13,6 +17,8 @@ pub enum PlayerHand {
 
 /// The hand the player is using to interact with
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FrogReadWrite)]
+#[cfg_attr(feature = "bevy", derive(bevy_reflect::Reflect))]
+#[cfg_attr(feature = "bevy", reflect(Default))]
 #[frog(tests = ["read_verify", "write_verify"], bytes = [0])]
 pub enum InteractionHand {
     /// The player's main hand
