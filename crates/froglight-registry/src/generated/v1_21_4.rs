ba5e56e6140231b54a56291011dcda16f57b11fe
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-02-15 17:30:14 -0800

    Bump dependencies

--- crates/froglight-registry/src/generated/v1_21_4.rs
+++ crates/froglight-registry/src/generated/v1_21_4.rs
@@ -3,7 +3,7 @@
 //! TODO: Documentation
 #![allow(missing_docs)]
 
-froglight_macros::registries! {
+froglight_macros::registry_values! {
     path = crate,
     feature = "v1_21_4",
     version = froglight_common::version::V1_21_4,
