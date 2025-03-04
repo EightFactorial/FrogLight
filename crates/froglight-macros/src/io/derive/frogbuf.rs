4f886a35a977ec3d104a32204c6086b932c6c81a
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-03-04 09:36:12 -0800

    Macro progress

--- crates/froglight-macros/src/io/derive/frogbuf.rs
+++ crates/froglight-macros/src/io/derive/frogbuf.rs
@@ -7,6 +7,18 @@ use syn::DeriveInput;
 
 use crate::CrateManifest;
 
+pub(crate) fn derive_frogbuf(input: TokenStream) -> TokenStream {
+    let input: DeriveInput = syn::parse2(input).unwrap();
+
+    let FrogBufMacro { .. } = FrogBufMacro::from_derive_input(&input).unwrap();
+    let DeriveInput { ident: _ident, generics: _generics, .. } = input;
+
+    let _path = CrateManifest::try_find("froglight-network", "froglight")
+        .unwrap_or_else(|| CrateManifest::froglight("froglight-io"));
+
+    quote! {}
+}
+
 #[derive(FromDeriveInput)]
 #[darling(attributes(frog))]
 struct FrogBufMacro {
@@ -21,16 +33,4 @@ enum MacroType {
 }
 impl Default for MacroType {
     fn default() -> Self { Self::ReadWrite { read: true, write: true } }
-}
-
-pub(crate) fn derive_frogbuf(input: TokenStream) -> TokenStream {
-    let input: DeriveInput = syn::parse2(input).unwrap();
-
-    let FrogBufMacro { .. } = FrogBufMacro::from_derive_input(&input).unwrap();
-    let DeriveInput { ident: _ident, generics: _generics, .. } = input;
-
-    let _path = CrateManifest::try_find("froglight-network", "froglight")
-        .unwrap_or_else(|| CrateManifest::froglight("froglight-io"));
-
-    quote! {}
 }
--- crates/froglight-macros/src/nbt/derive/frognbt.rs
+++ crates/froglight-macros/src/nbt/derive/frognbt.rs
@@ -1,3 +1,83 @@
+use darling::{FromField, FromVariant};
 use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, LitStr, Path};
 
-pub(crate) fn derive_frognbt(_input: TokenStream) -> TokenStream { TokenStream::new() }
+use crate::manifest::CrateManifest;
+
+pub(crate) fn derive_frognbt(input: TokenStream) -> TokenStream {
+    let input: DeriveInput = syn::parse2(input).unwrap();
+    let DeriveInput { ident, data, .. } = input;
+
+    let path = CrateManifest::froglight("froglight-nbt");
+
+    let from_tokens = TokenStream::new();
+    let as_tokens = TokenStream::new();
+
+    match data {
+        syn::Data::Struct(_data) => {}
+        syn::Data::Enum(_data) => {}
+        syn::Data::Union(..) => panic!("`FrogNbt` does not support unions!"),
+    }
+
+    quote! {
+        impl #path::convert::ConvertNbt for #ident {
+            fn from_compound(nbt: &#path::nbt::NbtCompound) -> Result<Self, #path::convert::ConvertError> {
+                #from_tokens
+            }
+
+            fn as_compound(&self) -> Result<#path::nbt::NbtCompound, #path::convert::ConvertError> {
+                #as_tokens
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, FromField)]
+#[darling(attributes(frog))]
+struct FrogNbtField {
+    tag: Option<LitStr>,
+    with: Option<Path>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, FromVariant)]
+#[darling(attributes(frog))]
+struct FrogNbtVariant {
+    tag: LitStr,
+    with: Option<Path>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum TagType {
+    Byte,
+    Short,
+    Int,
+    Long,
+    Float,
+    Double,
+    ByteArray,
+    String,
+    List,
+    Compound,
+    IntArray,
+    LongArray,
+}
+impl<'a> From<&'a str> for TagType {
+    fn from(value: &'a str) -> Self {
+        match value.to_lowercase().as_str() {
+            "byte" | "i8" | "u8" => TagType::Byte,
+            "short" | "i16" | "u16" => TagType::Short,
+            "int" | "integer" | "i32" | "u32" => TagType::Int,
+            "long" | "i64" | "u64" => TagType::Long,
+            "float" | "f32" => TagType::Float,
+            "double" | "f64" => TagType::Double,
+            "string" | "str" => TagType::String,
+            "list" | "vec" => TagType::List,
+            "compound" | "object" => TagType::Compound,
+            "bytearray" | "vec<i8>" | "vec<u8>" | "[i8]" | "[u8]" => TagType::ByteArray,
+            "intarray" | "vec<i32>" | "vec<u32>" | "[i32]" | "[u32]" => TagType::IntArray,
+            "longarray" | "vec<i64>" | "vec<u64>" | "[i64]" | "[u64]" => TagType::LongArray,
+            _ => panic!("Unknown tag type: \"{value}\""),
+        }
+    }
+}
