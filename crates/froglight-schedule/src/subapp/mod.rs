5117ec93bb5ff9fdc21c15cb699f738e42fd1d05
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-04-21 19:40:48 -0700

    Clean up `SubAppPlugin`
    Separated code into functions for readability

--- crates/froglight-schedule/src/subapp/mod.rs
+++ crates/froglight-schedule/src/subapp/mod.rs
@@ -27,99 +27,6 @@ pub struct SubAppPlugin<SubApp: AppLabel> {
     subapp_label: Mutex<Option<SubApp>>,
 }
 
-impl<SubAppLabel: AppLabel> Plugin for SubAppPlugin<SubAppLabel> {
-    fn build(&self, app: &mut App) {
-        assert!(
-            app.get_sub_app(self.subapp_label.lock().as_ref().unwrap().intern()).is_none(),
-            "`SubAppPlugin<{}>` tried to create a `SubApp` that already exists!",
-            std::any::type_name::<SubAppLabel>()
-        );
-
-        app.register_type::<SubWorlds>();
-
-        // Create a new subapp with the label.
-        let mut sub_app = SubApp::new();
-
-        {
-            // Copy select resources from the main app to the subapp.
-            sub_app.insert_resource(app.world().resource::<AppTypeRegistry>().clone());
-        }
-
-        {
-            // Set the subapp's extract function.
-            sub_app.set_extract(self.take_extract());
-        }
-
-        {
-            // Add the `Main` schedule and set it as the update schedule.
-            let mut main_schedule = Schedule::new(Main);
-            main_schedule.set_executor_kind(ExecutorKind::SingleThreaded);
-            sub_app.add_schedule(main_schedule).update_schedule = Some(Main.intern());
-
-            // Add the schedules in the order they will be run.
-            sub_app.insert_resource(MainScheduleOrder {
-                labels: vec![
-                    First.intern(),
-                    Network::PreNetwork.intern(),
-                    Tick::PreTick.intern(),
-                    Tick::Tick.intern(),
-                    Tick::PostTick.intern(),
-                    Network::PostNetwork.intern(),
-                    Last.intern(),
-                ],
-                ..Default::default()
-            });
-
-            // Add `bevy_time` support.
-            sub_app.init_resource::<Time>().register_type::<Time>();
-            sub_app.init_resource::<Time<Real>>().register_type::<Time<Real>>();
-            sub_app.init_resource::<Time<Virtual>>().register_type::<Time<Virtual>>();
-            sub_app.init_resource::<Time<Fixed>>().register_type::<Time<Fixed>>();
-            sub_app.init_resource::<TimeUpdateStrategy>().register_type::<Timer>();
-
-            // Add and initialize `CurrentTick`, `TickRate`, and `ShouldTick`.
-            sub_app.init_resource::<CurrentTick>().register_type::<CurrentTick>();
-            sub_app.init_resource::<TickRate>().register_type::<TickRate>();
-            sub_app.init_resource::<ShouldTick>().register_type::<ShouldTick>();
-
-            // Update the time and tick, run `Main` if needed.
-            sub_app.add_systems(
-                Main,
-                (
-                    time_system,
-                    ShouldTick::update,
-                    CurrentTick::increment_tick.run_if(ShouldTick::tick),
-                    Main::run_main.run_if(ShouldTick::tick),
-                )
-                    .chain(),
-            );
-
-            // Add the `SystemSetPlugin` to the subapp.
-            sub_app.add_plugins(SystemSetPlugin);
-        }
-
-        #[cfg(feature = "multi_threaded")]
-        {
-            // Setup the multi-threaded executor if not already set.
-            if !app.world().contains_resource::<ThreadedSubApps>() {
-                app.set_runner(ThreadedSubApps::threaded_runner);
-            }
-            // Add the label to the list of multi-threaded subapps.
-            app.world_mut()
-                .get_resource_or_init::<ThreadedSubApps>()
-                .push(self.subapp_label.lock().as_ref().unwrap().intern());
-        }
-
-        // Insert the subapp into the main app.
-        app.insert_sub_app(self.subapp_label.lock().take().unwrap(), sub_app);
-    }
-
-    fn finish(&self, app: &mut App) {
-        // Initialize a `SyncStorage` if it does not already exist.
-        app.init_resource::<AppSyncStorage<SubAppLabel>>();
-    }
-}
-
 impl<SubApp: AppLabel> SubAppPlugin<SubApp> {
     /// Creates a new [`SubAppPlugin`].
     #[inline]
@@ -130,7 +37,7 @@ impl<SubApp: AppLabel> SubAppPlugin<SubApp> {
 
     /// Set the [`SubApp`]'s extract function.
     #[must_use]
-    pub fn with_extract(self, extract: impl Fn(&mut World, &mut World) + Send + 'static) -> Self {
+    pub fn set_extract(&self, extract: impl Fn(&mut World, &mut World) + Send + 'static) -> &Self {
         *self.extract_fn.lock() = Some(Box::new(extract));
         self
     }
@@ -147,12 +54,118 @@ impl<SubApp: AppLabel> SubAppPlugin<SubApp> {
     /// Runs all registered [`SubAppSync`] functions from the [`SyncStorage`].
     pub fn default_extract(app: &mut World, sub: &mut World) {
         app.resource_scope::<AppSyncStorage<SubApp>, ()>(|app, storage| {
-            storage.iter().for_each(|sync| sync.sync(app, sub));
+            storage.iter().for_each(|reflect| reflect.sync(app, sub));
         });
     }
 }
 
 // -------------------------------------------------------------------------------------------------
+
+impl<SubAppLabel: AppLabel> Plugin for SubAppPlugin<SubAppLabel> {
+    fn build(&self, app: &mut App) {
+        let label = self.subapp_label.lock().take().unwrap().intern();
+
+        assert!(
+            app.get_sub_app(label).is_none(),
+            "`SubAppPlugin<{}>` tried to create a `SubApp` that already exists!",
+            std::any::type_name::<SubAppLabel>()
+        );
+
+        app.register_type::<SubWorlds>();
+
+        // Create a new subapp with the label.
+        let mut sub_app = SubApp::new();
+
+        // Copy the `AppTypeRegistry` into the subapp.
+        sub_app.insert_resource(app.world().resource::<AppTypeRegistry>().clone());
+        // Set the subapp's extract function.
+        sub_app.set_extract(self.take_extract());
+
+        // Add schedules and systems to the subapp.
+        add_schedules(&mut sub_app);
+        add_systems(&mut sub_app);
+
+        // Insert the subapp into the main app.
+        app.insert_sub_app(label, sub_app);
+
+        #[cfg(feature = "multi_threaded")]
+        {
+            // Setup the multi-threaded executor if not already set.
+            if !app.world().contains_resource::<ThreadedSubApps>() {
+                app.set_runner(ThreadedSubApps::threaded_runner);
+            }
+            // Add the label to the list of multi-threaded subapps.
+            app.world_mut().get_resource_or_init::<ThreadedSubApps>().push(label);
+        }
+    }
+
+    fn finish(&self, app: &mut App) {
+        // Initialize a `SyncStorage` if it does not already exist.
+        app.init_resource::<AppSyncStorage<SubAppLabel>>();
+    }
+}
+
+/// Add [`Schedule`]s to the [`SubApp`].
+fn add_schedules(sub_app: &mut SubApp) {
+    // Add the `Main` schedule and set it as the update schedule.
+    let mut main_schedule = Schedule::new(Main);
+    main_schedule.set_executor_kind(ExecutorKind::SingleThreaded);
+    sub_app.add_schedule(main_schedule).update_schedule = Some(Main.intern());
+
+    // Prepare the schedules and their executors.
+    let labels = vec![
+        (First.intern(), ExecutorKind::SingleThreaded),
+        (Network::PreNetwork.intern(), ExecutorKind::SingleThreaded),
+        (Tick::PreTick.intern(), ExecutorKind::default()),
+        (Tick::Tick.intern(), ExecutorKind::default()),
+        (Tick::PostTick.intern(), ExecutorKind::default()),
+        (Network::PostNetwork.intern(), ExecutorKind::SingleThreaded),
+        (Last.intern(), ExecutorKind::SingleThreaded),
+    ];
+
+    // Add all schedules and set their executor kind.
+    for (label, kind) in &labels {
+        let mut schedule = Schedule::new(*label);
+        schedule.set_executor_kind(*kind);
+        sub_app.add_schedule(schedule);
+    }
+
+    // Setup the `MainScheduleOrder` resource.
+    sub_app.insert_resource(MainScheduleOrder {
+        labels: labels.into_iter().map(|(l, _)| l).collect(),
+        ..Default::default()
+    });
+}
+
+/// Add [`System`]s to the [`SubApp`].
+fn add_systems(sub_app: &mut SubApp) {
+    // Add `bevy_time` resources.
+    sub_app.init_resource::<Time>().register_type::<Time>();
+    sub_app.init_resource::<Time<Real>>().register_type::<Time<Real>>();
+    sub_app.init_resource::<Time<Virtual>>().register_type::<Time<Virtual>>();
+    sub_app.init_resource::<Time<Fixed>>().register_type::<Time<Fixed>>();
+    sub_app.init_resource::<TimeUpdateStrategy>().register_type::<Timer>();
+
+    // Add `CurrentTick`, `TickRate`, and `ShouldTick`.
+    sub_app.init_resource::<CurrentTick>().register_type::<CurrentTick>();
+    sub_app.init_resource::<TickRate>().register_type::<TickRate>();
+    sub_app.init_resource::<ShouldTick>().register_type::<ShouldTick>();
+
+    // Update the time and tick, run `Main` if needed.
+    sub_app.add_systems(
+        Main,
+        (
+            time_system,
+            ShouldTick::update,
+            CurrentTick::increment_tick.run_if(ShouldTick::tick),
+            Main::run_main.run_if(ShouldTick::tick),
+        )
+            .chain(),
+    );
+
+    // Add the `SystemSetPlugin` to the subapp.
+    sub_app.add_plugins(SystemSetPlugin);
+}
 
 /// The system used to update the [`Time`] used by app logic.
 pub fn time_system(
