b6f264a00eb54d1aab34c7da09f226c542d3a360
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-05-16 18:53:17 -0700

    Finish `froglight-brigadier` "admin" example
    No, you probably shouldn't be checking *per command* whether the user has permission to run it. For this example it's enough to get the point across.
    
    Ideally you would have the permission levels for each command saved where they can be checked before commands are ever run.

--- crates/froglight-brigadier/examples/admin/commands.rs
+++ crates/froglight-brigadier/examples/admin/commands.rs
@@ -1,24 +1,82 @@
 use bevy::prelude::*;
-use froglight_brigadier::{function::WorldValueRef, prelude::*};
+use froglight_brigadier::{
+    argument::{BrigadierTail, BrigadierWord},
+    function::WorldValueRef,
+    prelude::*,
+};
 use froglight_entity::prelude::*;
 
 use crate::permissions::{PlayerPermissions, PlayerRole};
 
+/// A [`Plugin`] that builds and adds commands to the [`App`].
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct CommandsPlugin;
+pub(crate) struct CommandsPlugin;
 
 impl Plugin for CommandsPlugin {
     fn build(&self, app: &mut App) {
+        // Add a command to log who is running commands
+        app.add_command("announce", |builder| {
+            builder.command(|entity, mut world| {
+                let world = world.value();
+                let (name, _) = player_data(entity, &world);
+                info!("");
+                info!("{name}:");
+            });
+        });
+
+        // Add a command that adds two numbers together
+        app.add_command("add", |builder| {
+            let builder = builder.arg::<u32, _>().arg::<u32, _>();
+            builder.command(|_, a, b, _| {
+                info!("Adding   -> {a} + {b} = {}", a.saturating_add(b));
+            });
+        });
+
+        // Add a command that retrieves information from the `TypeRegistry`
+        app.add_command("reflect", |builder| {
+            let builder = builder.arg::<BrigadierWord, _>().arg::<BrigadierTail, _>();
+            builder.command(|entity, lookup_type, type_name, mut world| {
+                let world = world.value();
+
+                // Check if the player has permission to use this command
+                let (name, role) = player_data(entity, &world);
+                if role < PlayerRole::Moderator {
+                    warn!("{name} attempted to use the \"reflect\" command!");
+                    return;
+                }
+
+                // Get the type registration data
+                let registry = world.resource::<AppTypeRegistry>().read();
+                let type_data = match lookup_type.as_str() {
+                    "name" | "short" => registry.get_with_short_type_path(&type_name),
+                    "path" | "long" | "full" => registry.get_with_type_path(&type_name),
+                    unk => {
+                        error!("Unknown registry lookup: `{unk}`");
+                        return;
+                    }
+                };
+
+                // Print the type information
+                if let Some(type_data) = type_data {
+                    let type_path = type_data.type_info().type_path();
+                    let type_kind = type_data.type_info().kind();
+                    let type_id = type_data.type_id();
+
+                    info!("TypeInfo -> {type_kind:?} \"{type_path}\", {type_id:?}");
+                } else {
+                    error!("Type not found in registry: `{type_name}`");
+                }
+            });
+        });
+
         // Add a command to quit the application
         app.add_command("quit", |builder| {
             builder.command(|entity, mut world| {
                 let mut world = world.value();
-
-                let name = username_or_id(entity, &world);
-                let role = player_role(entity, &world);
+                let (name, role) = player_data(entity, &world);
 
                 if role >= PlayerRole::Admin {
-                    warn!("{name} has requested a shutdown!");
+                    info!("{name} has requested a shutdown!");
                     world.send_event(AppExit::Success);
                 } else {
                     warn!("{name} attempted to stop the server!");
@@ -28,19 +86,13 @@ impl Plugin for CommandsPlugin {
     }
 }
 
-/// Get the username of a player, or fallback to the entity id.
-fn username_or_id(entity: Entity, world: &WorldValueRef) -> String {
+/// Get the username and role of the player
+fn player_data(entity: Entity, world: &WorldValueRef) -> (String, PlayerRole) {
     match world.get::<PlayerProfile>(entity) {
-        Some(profile) => profile.username.to_string(),
-        None => format!("Entity {entity}"),
-    }
-}
-
-/// Get the player's role.
-fn player_role(entity: Entity, world: &WorldValueRef) -> PlayerRole {
-    if let Some(uuid) = world.get::<PlayerUuid>(entity) {
-        world.resource::<PlayerPermissions>().get(&**uuid).copied().unwrap_or_default()
-    } else {
-        PlayerRole::default()
+        Some(profile) => {
+            let role = world.resource::<PlayerPermissions>().get(&*profile.uuid);
+            (profile.username.to_string(), role.copied().unwrap_or_default())
+        }
+        None => (format!("Entity {entity}"), PlayerRole::default()),
     }
 }
