4e15dbb71b631ded9966f03b2f9d463f8e7e2b75
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-06-04 16:21:34 -0700

    Allow NBT wrappers to actually be constructed
    Oops :)

--- crates/froglight-nbt/src/io/wrapper.rs
+++ crates/froglight-nbt/src/io/wrapper.rs
@@ -13,7 +13,31 @@ use crate::{
 /// Allows reading and writing NBT data using
 /// the [`FrogRead`] and [`FrogWrite`] traits.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Deref, DerefMut, AsRef, AsMut)]
-pub struct NbtWrapper<T: FromTag + IntoTag>(T);
+pub struct NbtWrapper<T: FromTag + IntoTag>(pub T);
+
+impl<T: FromTag + IntoTag> NbtWrapper<T> {
+    /// Wraps the given value in a [`NbtWrapper`].
+    #[inline]
+    #[must_use]
+    pub const fn new(value: T) -> Self { Self(value) }
+
+    /// Get a reference to the inner value.
+    #[inline]
+    #[must_use]
+    pub const fn inner(&self) -> &T { &self.0 }
+
+    /// Get a mutable reference to the inner value.
+    #[inline]
+    #[must_use]
+    pub const fn inner_mut(&mut self) -> &mut T { &mut self.0 }
+
+    /// Unwraps the value from the [`NbtWrapper`].
+    #[inline]
+    #[must_use]
+    pub fn into_inner(self) -> T { self.0 }
+}
+
+// -------------------------------------------------------------------------------------------------
 
 impl<T: FromTag + IntoTag> FrogRead for NbtWrapper<T> {
     fn frog_read(buffer: &mut impl Read) -> Result<Self, ReadError> {
@@ -40,7 +64,31 @@ impl<T: FromTag + IntoTag> FrogWrite for NbtWrapper<T> {
 /// Allows reading and writing NBT data using
 /// the [`FrogRead`] and [`FrogWrite`] traits.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Deref, DerefMut, AsRef, AsMut)]
-pub struct NbtTagWrapper<T: FromCompound + IntoCompound>(T);
+pub struct NbtTagWrapper<T: FromCompound + IntoCompound>(pub T);
+
+impl<T: FromCompound + IntoCompound> NbtTagWrapper<T> {
+    /// Wraps the given value in a [`NbtTagWrapper`].
+    #[inline]
+    #[must_use]
+    pub const fn new(value: T) -> Self { Self(value) }
+
+    /// Get a reference to the inner value.
+    #[inline]
+    #[must_use]
+    pub const fn inner(&self) -> &T { &self.0 }
+
+    /// Get a mutable reference to the inner value.
+    #[inline]
+    #[must_use]
+    pub const fn inner_mut(&mut self) -> &mut T { &mut self.0 }
+
+    /// Unwraps the value from the [`NbtTagWrapper`].
+    #[inline]
+    #[must_use]
+    pub fn into_inner(self) -> T { self.0 }
+}
+
+// -------------------------------------------------------------------------------------------------
 
 impl<T: FromCompound + IntoCompound> FrogRead for NbtTagWrapper<T> {
     fn frog_read(buffer: &mut impl Read) -> Result<Self, ReadError> {
