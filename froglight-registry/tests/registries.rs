1a3301e3159ab3341e4be4e018668963e8af54f1
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2026-01-04 15:26:25 -0800

    chore: Fix more broken features
    Glad I caught this too. Pretty nice that with all features disabled and only `std` there's only about 20 dependencies total.

--- froglight-registry/tests/registries.rs
+++ froglight-registry/tests/registries.rs
@@ -60,10 +60,11 @@ fn entry_a() {
 }
 
 #[test]
+#[allow(unused_mut, reason = "Used if `alloc` feature is enabled")]
 #[cfg(any(feature = "async", feature = "parking_lot", feature = "std"))]
 fn entry_b() {
     let mut storage = TestVersion::registries().write();
-    let registry = storage.get_mut("test:example_b").unwrap();
+    let registry = storage.get("test:example_b").unwrap();
 
     assert_eq!(registry.len(), 2);
     assert_eq!(registry.get_index("test:example_b_1"), Some(0));
@@ -71,13 +72,17 @@ fn entry_b() {
     assert_eq!(registry.get_index("test:example_b_2"), Some(1));
     assert_eq!(registry.get_name(1).unwrap(), "test:example_b_2");
 
-    registry.to_mut().push(Identifier::new_static("test:example_b_3"));
+    #[cfg(feature = "alloc")]
+    {
+        let registry = storage.get_mut("test:example_b").unwrap();
+        registry.to_mut().push(Identifier::new_static("test:example_b_3"));
 
-    assert_eq!(registry.len(), 3);
-    assert_eq!(registry.get_index("test:example_b_1"), Some(0));
-    assert_eq!(registry.get_name(0).unwrap(), "test:example_b_1");
-    assert_eq!(registry.get_index("test:example_b_2"), Some(1));
-    assert_eq!(registry.get_name(1).unwrap(), "test:example_b_2");
-    assert_eq!(registry.get_index("test:example_b_3"), Some(2));
-    assert_eq!(registry.get_name(2).unwrap(), "test:example_b_3");
+        assert_eq!(registry.len(), 3);
+        assert_eq!(registry.get_index("test:example_b_1"), Some(0));
+        assert_eq!(registry.get_name(0).unwrap(), "test:example_b_1");
+        assert_eq!(registry.get_index("test:example_b_2"), Some(1));
+        assert_eq!(registry.get_name(1).unwrap(), "test:example_b_2");
+        assert_eq!(registry.get_index("test:example_b_3"), Some(2));
+        assert_eq!(registry.get_name(2).unwrap(), "test:example_b_3");
+    }
 }
