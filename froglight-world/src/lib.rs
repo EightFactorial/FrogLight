7beac7add5cb18647ff286233754dc7caf3a694c
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2026-01-07 01:20:05 -0800

    chore: Misc

--- froglight-world/src/lib.rs
+++ froglight-world/src/lib.rs
@@ -11,24 +11,23 @@ pub mod component;
 pub mod section;
 
 /// The length of a chunk.
-pub const CHUNK_LENGTH: u8 = 16;
+const CHUNK_LENGTH: u8 = 16;
 /// The width of a chunk.
-pub const CHUNK_WIDTH: u8 = 16;
+const CHUNK_WIDTH: u8 = 16;
 
 /// The length of a section.
-pub const SECTION_LENGTH: u8 = CHUNK_LENGTH;
+const SECTION_LENGTH: u8 = CHUNK_LENGTH;
 /// The width of a section.
-pub const SECTION_WIDTH: u8 = CHUNK_WIDTH;
+const SECTION_WIDTH: u8 = CHUNK_WIDTH;
 /// The height of a section.
-pub const SECTION_HEIGHT: u8 = 16;
+const SECTION_HEIGHT: u8 = 16;
 /// The volume of a section.
-pub const SECTION_VOLUME: u16 =
-    SECTION_LENGTH as u16 * SECTION_WIDTH as u16 * SECTION_HEIGHT as u16;
+const SECTION_VOLUME: u16 = SECTION_LENGTH as u16 * SECTION_WIDTH as u16 * SECTION_HEIGHT as u16;
 
 pub mod prelude {
     //! Re-exports of common types, traits, and macros.
 
     #[cfg(feature = "alloc")]
-    pub use crate::chunk::Chunk;
+    pub use crate::chunk::{Chunk, SharedChunk};
     pub use crate::component::{BlockPos, ChunkPos};
 }
