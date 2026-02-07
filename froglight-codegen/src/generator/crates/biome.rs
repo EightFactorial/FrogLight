d0ac6a64f739e5f194833f0490a8e80ae396f741
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2026-02-07 12:49:10 -0800

    chore: Extract and generate partial biome data
    Added precipitation, temperature, and downfall data.
    
    Still missing biome colors, attributes, and features, but the work for them is already mostly done

--- froglight-codegen/src/generator/crates/biome.rs
+++ froglight-codegen/src/generator/crates/biome.rs
@@ -5,6 +5,8 @@ use cafebabe::{
     constant_pool::{LiteralConstant, Loadable, MemberRef},
 };
 use convert_case::{Case, Casing};
+use facet::Facet;
+use facet_value::Value;
 use indexmap::{IndexMap, IndexSet};
 use miette::Result;
 use tokio::sync::{OnceCell, RwLock};
@@ -13,12 +15,22 @@ use crate::{
     common::{DATA, Version, VersionStorage, WORKSPACE_DIR},
     config::{ConfigBundle, VersionPair},
     helper::{ModuleBuilder, VersionHelper},
-    source::JarData,
+    source::{JarData, JarFile},
 };
 
 #[derive(Debug, Clone)]
 pub struct BiomeData {
-    pub biomes: IndexMap<String, String>,
+    pub biomes: IndexMap<String, BiomeSettings>,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct BiomeSettings {
+    pub ident: String,
+    pub temperature: f64,
+    pub downfall: f64,
+    pub precipitation: bool,
+    pub attr: Vec<String>,
+    pub feat: Vec<Vec<String>>,
 }
 
 impl BiomeData {
@@ -71,7 +83,10 @@ impl BiomeData {
                         if let Some(constant) = constant.take() {
                             biomes.insert(
                                 name_and_type.name.to_string(),
-                                format!("minecraft:{constant}"),
+                                BiomeSettings {
+                                    ident: format!("minecraft:{constant}"),
+                                    ..Default::default()
+                                },
                             );
                         } else {
                             tracing::warn!(
@@ -88,10 +103,49 @@ impl BiomeData {
         })
         .await?;
 
+        JarFile::get_for(version, storage, async |file| {
+            let directory = file.generated.join("data/minecraft/worldgen/biome");
+            for biome in biomes.values_mut() {
+                let mut path = directory.join(biome.ident.trim_start_matches("minecraft:"));
+                path.set_extension("json");
+
+                let Ok(content) = tokio::fs::read_to_string(&path).await else {
+                    tracing::warn!("Failed to read biome file: {}", path.display());
+                    continue;
+                };
+
+                match facet_json::from_str::<BiomeJson>(&content) {
+                    Ok(json) => {
+                        biome.temperature = json.temperature;
+                        biome.downfall = json.downfall;
+                        biome.precipitation = json.has_precipitation;
+                        biome.feat = json.features;
+                    }
+                    Err(err) => {
+                        miette::bail!("Failed to parse \"{}\" JSON, {err}", biome.ident);
+                    }
+                }
+            }
+
+            Ok(())
+        })
+        .await?;
+
         tracing::debug!("Found {} biomes for \"{}\"", biomes.len(), version.as_str());
 
         Ok(BiomeData { biomes })
     }
+}
+
+#[derive(Debug, Clone, Facet)]
+struct BiomeJson {
+    #[facet(default)]
+    attributes: Value,
+    downfall: f64,
+    effects: Value,
+    features: Vec<Vec<String>>,
+    has_precipitation: bool,
+    temperature: f64,
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -169,14 +223,14 @@ pub async fn generate(version: &VersionPair, storage: &mut VersionStorage) -> Re
                 content.push_str(&version_type);
                 content.push_str(",\n");
 
-                for (index, (biome, ident)) in data.biomes.iter().enumerate() {
+                for (index, (biome, settings)) in data.biomes.iter().enumerate() {
                     content.push_str("    ");
                     content.push_str(&biome.to_case(Case::Pascal));
                     content.push_str(" => { ident: \"");
-                    content.push_str(ident);
+                    content.push_str(&settings.ident);
                     content.push_str("\", global: ");
                     content.push_str(&index.to_string());
-                    content.push_str(", prop: { foliage: 0, grass: 0, water: 0, precip: true, temp: 0.0, downfall: 0.0 },\n");
+                    writeln!(content, ", prop: {{ foliage: {}, grass: {}, water: {}, precip: {}, temp: {}f32, downfall: {}f32 }},", 0, 0, 0, settings.precipitation, settings.temperature, settings.downfall).unwrap();
                     content.push_str("        attr: BiomeAttributeSet::empty(), feat: BiomeFeatureSet::empty() }");
                     if index != data.biomes.len() - 1 {
                         content.push(',');
--- froglight-codegen/src/source/jarfile.rs
+++ froglight-codegen/src/source/jarfile.rs
@@ -1,7 +1,8 @@
-use std::io::Cursor;
+use std::{io::Cursor, path::PathBuf, process::Stdio};
 
 use async_zip::tokio::read::seek::ZipFileReader;
 use miette::Result;
+use tokio::process::Command;
 
 use crate::{
     common::{CACHE_DIR, REQWEST, Version, VersionStorage},
@@ -9,6 +10,10 @@ use crate::{
 };
 
 pub struct JarFile {
+    pub client: PathBuf,
+    pub server: PathBuf,
+    pub generated: PathBuf,
+
     pub raw: &'static Vec<u8>,
     pub reader: ZipFileReader<Cursor<&'static [u8]>>,
 }
@@ -32,52 +37,74 @@ impl JarFile {
     /// Fetch the [`JarFile`] for the given [`Version`].
     pub async fn fetch(version: &Version, storage: &mut VersionStorage) -> Result<Self> {
         let mut path = CACHE_DIR.clone();
-        let jar = format!("{}.jar", version.as_str());
         path.push(version.as_feature());
-        path.push(&jar);
 
-        let content = if path.exists() {
+        let client = path.join(format!("{}_client.jar", version.as_str()));
+        let server = path.join(format!("{}_server.jar", version.as_str()));
+
+        let content = if client.exists() && server.exists() {
             tracing::debug!("Using cached `JarFile` for \"{}\"", version.as_str());
 
-            match tokio::fs::read(&path).await {
+            match tokio::fs::read(&client).await {
                 Ok(content) => content,
                 Err(_err) => todo!(),
             }
         } else {
             tracing::debug!("Downloading `JarFile` for \"{}\"", version.as_str());
 
-            let content = VersionData::get_for(version, storage, async |data| {
-                let response = match REQWEST.get(&data.downloads.client.url).send().await {
-                    Ok(response) => response,
-                    Err(_err) => todo!(),
-                };
-                match response.bytes().await {
-                    Ok(bytes) => Ok(bytes.to_vec()),
-                    Err(_err) => todo!(),
+            VersionData::get_for(version, storage, async |data| {
+                for (index, url) in
+                    [&data.downloads.server.url, &data.downloads.client.url].into_iter().enumerate()
+                {
+                    let response = match REQWEST.get(url).send().await {
+                        Ok(response) => response,
+                        Err(_err) => todo!(),
+                    };
+                    let content = match response.bytes().await {
+                        Ok(bytes) => bytes.to_vec(),
+                        Err(_err) => todo!(),
+                    };
+
+                    // Ensure parent directory exists
+                    let path = if index == 0 { &server } else { &client };
+                    tracing::debug!("Caching `JarFile` for \"{}\" at {:?}", version.as_str(), path);
+                    if let Some(parent) = path.parent()
+                        && !parent.exists()
+                        && let Err(_err) = tokio::fs::create_dir_all(parent).await
+                    {
+                        todo!()
+                    }
+
+                    // Write to cache
+                    if let Err(_err) = tokio::fs::write(path, &content).await {
+                        todo!()
+                    }
+
+                    if index == 1 {
+                        return Ok(content);
+                    }
                 }
+                unreachable!();
             })
-            .await?;
-
-            // Ensure parent directory exists
-            tracing::debug!("Caching `JarFile` for \"{}\" at {:?}", version.as_str(), path);
-            if let Some(parent) = path.parent()
-                && !parent.exists()
-                && let Err(_err) = tokio::fs::create_dir_all(parent).await
-            {
-                todo!()
-            }
-
-            // Write to cache
-            if let Err(_err) = tokio::fs::write(&path, &content).await {
-                todo!()
-            }
-
-            content
+            .await?
         };
 
-        let leaked: &'static Vec<u8> = Box::leak(Box::new(content));
-        match ZipFileReader::with_tokio(Cursor::new(leaked.as_slice())).await {
-            Ok(reader) => Ok(Self { raw: leaked, reader }),
+        // Run the data generator if necessary.
+        let mut generated = server.clone();
+        generated.set_file_name("generated");
+
+        if !generated.exists() {
+            let mut command = Command::new("java");
+            command.current_dir(server.parent().unwrap());
+            command.args(["-DbundlerMainClass=net.minecraft.data.Main", "-jar"]);
+            command.arg(&server).arg("--all");
+            let mut task = command.stdout(Stdio::null()).spawn().unwrap();
+            task.wait().await.unwrap();
+        }
+
+        let raw: &'static Vec<u8> = Box::leak(Box::new(content));
+        match ZipFileReader::with_tokio(Cursor::new(raw.as_slice())).await {
+            Ok(reader) => Ok(Self { client, server, generated, raw, reader }),
             Err(_err) => todo!(),
         }
     }
