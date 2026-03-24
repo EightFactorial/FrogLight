8e988ef467d37cf592530558c9e9b2b5417644cf
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2026-03-24 10:07:04 -0700

    chore: Bump to 26.1
    Yippee!

--- froglight-common/src/version/generated.rs
+++ froglight-common/src/version/generated.rs
@@ -13,7 +13,7 @@ pub struct V26_1;
 
 #[cfg(feature = "v26_1")]
 impl super::Version for V26_1 {
-    const DATA_VERSION: u32 = 4784;
-    const PROTOCOL_ID: u32 = 1073742126;
+    const DATA_VERSION: u32 = 4786;
+    const PROTOCOL_ID: u32 = 775;
     const RESOURCE_VERSION: u32 = 84;
 }
